a330d173352be37f6eef402a3484c2ed51ce753d
Halip26 <77138807+Halip26@users.noreply.github.com>
2025-12-23 10:58:44 +0800

    change the path directory

--- wsqi.py
+++ wsqi.py
@@ -2,8 +2,8 @@
 import sys
 import os
 
-# Tambahkan path proyek jika perlu:
-project_path = '/home/Halip26/quiz-app-kodland'
+# add project path as needed:
+project_path = "/home/Halip26/quiz-app"
 if project_path not in sys.path:
     sys.path.append(project_path)
 
